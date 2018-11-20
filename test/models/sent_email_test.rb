d1ca1beae1b2696ac0f8e7556f9ae0791392a9e9
Shaun Adams <38860831+Adams1991@users.noreply.github.com>
2018-11-20 12:20:06 +0000

    CRUD functions tested on SentEmail model

--- test/models/sent_email_test.rb
+++ test/models/sent_email_test.rb
@@ -10,4 +10,21 @@ class SentEmailTest < ActiveSupport::TestCase
     assert_equal 'Bob' , result.recipient_name
   end
 
+  test 'if recipient_email is being saved' do
+    result = SentEmail.find_by(recipient_email:'bob@example.com')
+    assert_equal 'bob@example.com' , result.recipient_email
+  end
+
+  test 'if subject is being saved' do
+    result = SentEmail.find_by(subject:'Test Email')
+    assert_equal 'Test Email' , result.subject
+  end
+
+
+  test 'if body is being saved' do
+    result = SentEmail.find_by(body:'We need to test if this saves')
+    assert_equal 'We need to test if this saves' , result.body
+  end
+
+
 end
