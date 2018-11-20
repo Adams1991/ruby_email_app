8822d037e523e1a814192b2e6fb9e25843d06cf4
Shaun Adams <38860831+Adams1991@users.noreply.github.com>
2018-11-20 12:12:50 +0000

    begins testing SentEmail model CRUD functions

--- test/models/sent_email_test.rb
+++ test/models/sent_email_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class SentEmailTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+     @sentEmail = SentEmail.create(recipient_name:'Bob', recipient_email: 'bob@example.com', subject: "Test Email", body: "We need to test if this saves")
+  end
+
+  test 'if recipient_name is being saved' do
+    result = SentEmail.find_by(recipient_name:'Bob')
+  end
+
 end
