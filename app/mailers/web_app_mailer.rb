0fb3ae3ad3a124558d473954fe6e913d0171d4ca
Shaun Adams <38860831+Adams1991@users.noreply.github.com>
2018-11-20 13:23:55 +0000

    mailer rename

---
+++ app/mailers/web_app_mailer.rb
@@ -0,0 +1,2 @@
+class WebAppMailer < ApplicationMailer
+end
---
+++ test/mailers/previews/web_app_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/web_app_mailer
+class WebAppMailerPreview < ActionMailer::Preview
+
+end
