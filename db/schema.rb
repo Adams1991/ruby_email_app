16446983f2784232f40370175d0e52fcd00b6b61
Shaun Adams <38860831+Adams1991@users.noreply.github.com>
2018-11-20 12:42:14 +0000

    Needed CRUD functions for EmailTemplate model passing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181120115932) do
+ActiveRecord::Schema.define(version: 20181120123831) do
+
+  create_table "email_templates", force: :cascade do |t|
+    t.string "subject"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sent_emails", force: :cascade do |t|
     t.string "recipient_name"
--- test/models/email_template_test.rb
+++ test/models/email_template_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class EmailTemplateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+     @emailTemplate = EmailTemplate.create(subject: "Test Email", body: "We need to test if this saves")
+  end
+
+  test 'if subject is being saved' do
+    result = EmailTemplate.find_by(subject:'Test Email')
+    assert_equal 'Test Email' , result.subject
+  end
+
+
+  test 'if body is being saved' do
+    result = EmailTemplate.find_by(body:'We need to test if this saves')
+    assert_equal 'We need to test if this saves' , result.body
+  end
+
 end
