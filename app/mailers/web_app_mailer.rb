4bd2c857ecf457513ec2ca2d350a5f2ffadb370c
Shaun Adams <38860831+Adams1991@users.noreply.github.com>
2018-11-20 15:33:56 +0000

    issue with mailer not finding template

--- app/mailers/web_app_mailer.rb
+++ app/mailers/web_app_mailer.rb
@@ -1,2 +1,6 @@
 class WebAppMailer < ApplicationMailer
+  def mail_method(data,email,subject)
+    @body=data
+    mail(to: email, subject: subject)
+  end
 end
--- test/mailers/web_app_mailer_test.rb
+++ test/mailers/web_app_mailer_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class WebAppMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+     @email = WebAppMailer.mail_method("This is the body", "example@example.com", "this is the subject").deliver_now
+  end
+
+  test 'email is sent' do
+    assert !ActionMailer::Base.deliveries.empty?
+  end
+
 end
