b3c5591dae30ffe6a03beeda32238bd6e878c1e8
Shaun Adams <38860831+Adams1991@users.noreply.github.com>
2018-11-20 15:57:09 +0000

    mailer passing initial tests

--- test/mailers/web_app_mailer_test.rb
+++ test/mailers/web_app_mailer_test.rb
@@ -2,11 +2,23 @@ require 'test_helper'
 
 class WebAppMailerTest < ActionMailer::TestCase
   def setup
-     @email = WebAppMailer.web_app_email("This is the body", "example@example.com", "this is the subject").deliver_now
+     @email = WebAppMailer.web_app_email("This is the body", "example@example.com", "this is the subject")
   end
 
-  test 'email is sent' do
+  test 'email is not in queue when not sent' do
+    assert ActionMailer::Base.deliveries.empty?
+  end
+
+  test 'email can be sent' do
+    @email.deliver_now
     assert !ActionMailer::Base.deliveries.empty?
+  end
+
+  test 'sent email has correct contents' do
+      @email.deliver_now
+      assert_equal ['example@example.com'], @email.to
+      assert_equal "this is the subject", @email.subject
+      assert_equal "This is the body\n\n", @email.text_part.body.to_s 
   end
 
 end
