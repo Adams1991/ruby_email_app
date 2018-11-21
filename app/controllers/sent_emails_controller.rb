9f3e036b7a77d56b0bc872eb1aadfd2d79c16485
Shaun Adams <38860831+Adams1991@users.noreply.github.com>
2018-11-21 15:01:56 +0000

    able to save emails via browser

--- app/controllers/sent_emails_controller.rb
+++ app/controllers/sent_emails_controller.rb
@@ -4,7 +4,11 @@ class SentEmailsController < ApplicationController
   end
 
   def create
-
+       body = params[:message_preview]
+       subject =params[:subject]
+       recipient_name = params[:name]
+       recipient_email = params[:email]
+       SentEmail.create(recipient_name: recipient_name, recipient_email: recipient_email, subject: subject, body: body )
   end
 
 end
