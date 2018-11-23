b3f687f159d20d4fb5554232bfb5e8246bd79f3f
Shaun Adams <38860831+Adams1991@users.noreply.github.com>
2018-11-23 15:28:05 +0000

    still no rendering with AJAX

--- app/controllers/sent_emails_controller.rb
+++ app/controllers/sent_emails_controller.rb
@@ -5,30 +5,29 @@ class SentEmailsController < ApplicationController
   end
 
   def show
-    @selected_template = EmailTemplate.find_by("id = ?", params[:email_template_id])
-    @email_templates = EmailTemplate.all
-  end
+   @selected_template = EmailTemplate.find_by("id = ?", params[:email_template_id])
+ end
 
-  def update_form
-      @selected_template = EmailTemplate.find_by("template_id = ?", params[:email_template_id])
-      respond_to do |format|
-        format.js
-      end
-  end
+ def update_form
+     @selected_template = EmailTemplate.find_by("template_id = ?", params[:email_template_id])
+     respond_to do |format|
+       format.js
+     end
+ end
 
 
   def create
-      # byebug
        sent_email_hash = params[:sent_email]
 
        body = sent_email_hash[:message_preview]
        subject = sent_email_hash[:subject]
        recipient_name = sent_email_hash[:name]
        recipient_email = sent_email_hash[:email]
-       
+
        email = WebAppMailer.web_app_email(body, recipient_email, subject)
        email.deliver_now
        SentEmail.create(recipient_name: recipient_name, recipient_email: recipient_email, subject: subject, body: body )
+
   end
 
 end
